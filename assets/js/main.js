348acfd43309ee2d95a6eba35df5be1bba89aee7
aditiam <aditiamuchlis23@gmail.com>
2021-08-29 09:30:02 +0700

    Rename result download CV from HTML to PDF in javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,7 +99,7 @@ let resumeButton = document.getElementById('resume-button')
 // Html2pdf options
 let opt = {
     margin              : 0,
-    filename            : 'resumeAditia.pdf',
+    filename            : 'Aditia Muchlis-Curriculum Vitae.pdf',
     image               : { type: 'jpeg', quality: 0.98 },
     html2canvas         : { scale: 4 },
     jsPDF               : { format: 'a4', orientation: 'portrait' },
